f29efc715f9bdf9767dc4bc92f8766c1b6102ef0
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-04-11 10:50:24 -0400

    feat: Init with create-flayyer-app

---
+++ flayyer.config.js
@@ -0,0 +1,12 @@
+const {config} = require("@flayyer/flayyer-types");
+require("dotenv").config();
+
+module.exports = config({
+  engine: "react-typescript",
+  key: process.env.FLAYYER_KEY,
+  deck: "simple-fade",
+
+  // Optionals
+  name: "My Deck",
+  description: "Created with create-flayyer-app",
+});
