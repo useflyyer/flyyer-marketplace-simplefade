84ca1a1787317d364c78c99fe7c257295f21fe60
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-04-23 10:32:52 -0400

    chore: Update flayyer.config.js

--- flayyer.config.js
+++ flayyer.config.js
@@ -1,4 +1,5 @@
-const {config} = require('@flayyer/flayyer-types');
+const {config, Sizes} = require('@flayyer/flayyer-types');
+const dedent = require("dedent")
 require('dotenv').config();
 
 module.exports = config({
@@ -7,6 +8,16 @@ module.exports = config({
   deck: 'simple-fade',
 
   // Optionals
-  name: 'My Deck',
-  description: 'Created with create-flayyer-app'
+  name: 'Simple Fade',
+  description: dedent`
+    Ideal for news or blogpost with a featured image.
+    Automatically centers the image base on the point of interest and uses the best white/dark scheme depending on the contract with the featured image.
+  `,
+  marketplace: true,
+  homepage: "https://flayyer.com",
+  keywords: ["flayyer", "free", "fade", "contrast"],
+  license: "MIT",
+  private: false,
+  repository: "https://github.com/flayyer/flayyer-marketplace-simplefade",
+  sizes: ["THUMBNAIL", "BANNER", "SQUARE", "STORY", "FREE"],
 });
